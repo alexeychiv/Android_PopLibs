1460da496930390bab7d4d4b51bb3fc5b8088524
alexeychiv <alexius-ch@yandex.ru>
2021-12-12 21:28:06 +0300

    GlideImageLoader.kt fix

--- app/src/main/java/gb/android/android_poplibs/ui/imageloading/GlideImageLoader.kt
+++ app/src/main/java/gb/android/android_poplibs/ui/imageloading/GlideImageLoader.kt
@@ -1,14 +1,44 @@
 package gb.android.android_poplibs.ui.imageloading
 
+import android.graphics.Bitmap
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 
 class GlideImageLoader: ImageLoader<ImageView> {
 
     override fun loadInto(url: String, container: ImageView) {
         Glide.with(container.context)
+            .asBitmap()
             .load(url)
+            .circleCrop()
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .listener(object: RequestListener<Bitmap> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+            })
             .into(container)
     }
 
